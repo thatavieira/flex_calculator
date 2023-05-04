6ab609f19a3e74489de5b5f848c3eb4d022cd971
thatavieira <thagurgel0909@gmail.com>
2023-05-04 09:19:02 -0300

    wip:nput form screen Abastecimento

--- src/pages/Abastecimento.js
+++ src/pages/Abastecimento.js
@@ -1,4 +1,6 @@
-import { StyleSheet } from "react-native";
+import { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { RadioButton, Text } from "react-native-paper";
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import Body from "../components/Body";
@@ -11,6 +13,9 @@ const Abastecimento = () => {
 
     const navigation = useNavigation();
 
+    const [tipo, setTipo] = useState('gas');
+
+
     return (
         <SafeAreaProvider>
             <Container>
@@ -19,6 +24,30 @@ const Abastecimento = () => {
                     goBack={() => navigation.goBack()}
                 />
                 <Body>
+
+                    <View style={styles.containerRadio}>
+
+                        <View style={styles.containerItem}>
+                            <RadioButton
+                                value="first"
+                                status={tipo === 'gas' ? 'checked' : 'unchecked'}
+                                color={'red'}
+                                onPress={() => setTipo('gas')}
+                            />
+                            <Text>Gasolina</Text>
+                        </View>
+
+                        <View style={styles.containerItem}>
+                            <RadioButton
+                                value="second"
+                                status={tipo === 'eta' ? 'checked' : 'unchecked'}
+                                color={'green'}
+                                onPress={() => setTipo('eta')}
+                            />
+                            <Text>Etanol</Text>
+                        </View>
+                    </View>
+
                 </Body>
             </Container>
         </SafeAreaProvider>
@@ -26,7 +55,18 @@ const Abastecimento = () => {
 };
 
 const styles = StyleSheet.create({
+    containerRadio: {
+        flexDirection: 'row',
+        margin: 8,
+        alignItems: 'center',
+        justifyContent: 'space-evenly'
+    },
 
+    containerItem: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-evenly'
+    },
 });
 
 export default Abastecimento;
