8deef2e904a4e4363d89cde7bef35b4d83e9c0b6
thatavieira <thagurgel0909@gmail.com>
2023-05-06 00:17:39 -0300

    install packages

--- src/navigations/Main.js
+++ src/navigations/Main.js
@@ -8,7 +8,7 @@ const Stack = createNativeStackNavigator();
 
 const Main = () => {
     return (
-        <Stack.Navigator initialRouteName="Abastecimento">
+        <Stack.Navigator initialRouteName="Home">
             <Stack.Screen
                 name="Home"
                 component={Home}
