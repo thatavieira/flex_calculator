ad08bd507be26ca84e1357c269ebeff0e5f658fc
thatavieira <thagurgel0909@gmail.com>
2023-05-19 18:27:28 -0300

    wip: function register and auth

---
+++ src/pages/Register.js
@@ -0,0 +1,87 @@
+import { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Button, Headline, TextInput } from 'react-native-paper';
+
+import Body from '../components/Body';
+import Container from '../components/Container';
+import Input from '../components/Input';
+import Logo from '../components/Logo';
+
+import { useNavigation } from '@react-navigation/native';
+
+const Register = () => {
+
+    const navigation = useNavigation();
+
+    const [name, setName] = useState('Thais Gurgel');
+
+    const [email, setEmail] = useState('pucminas');
+
+    const [password, setPassword] =
+        useState('pucminas');
+
+
+    return (
+        <Container>
+            <View style={styles.header}>
+                <Logo />
+            </View>
+
+            <Headline style={styles.textHeader}>Gerenciador de Combustivel</Headline>
+
+            <Body>
+                <Input
+                    label="Nome"
+                    value={name}
+                    onChangeText={(text) =>
+                        setName(text)}
+                    left={<TextInput.Icon name="account" />}
+                />
+                <Input
+                    label="Email"
+                    value={email}
+                    onChangeText={(text) =>
+                        setEmail(text)}
+                    left={<TextInput.Icon
+                        name="email" />}
+                />
+                <Input
+                    label="Senha"
+                    value={password}
+                    secureTextEntry
+                    onChangeText={(text) =>
+                        setPassword(text)}
+                    left={<TextInput.Icon name="Key" />}
+                />
+                <Button
+                    style={styles.button}
+                    mode="contained"
+                    onPress={() => console.log('Pressed')}>
+                    REGISTRAR
+                </Button>
+                <Button
+                    style={styles.button}
+                    mode="outlined"
+                    onPress={() => navigation.goBack()}>
+                    Cancelar
+                </Button>
+            </Body>
+        </Container>
+    );
+};
+
+const styles = StyleSheet.create({
+    button: {
+        marginBottom: 8,
+    },
+    textHeader: {
+        textAlign: 'center',
+    },
+    header: {
+        alignItems: 'center',
+        marginTop: 30,
+        marginBottom: 12
+    },
+});
+
+export default Register;
